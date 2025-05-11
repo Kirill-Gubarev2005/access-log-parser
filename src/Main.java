e0b64ae8f8128f6ec30dbd7559b656cfc17c12da
Kirill Gubarev <klg05@mail.ru>
2025-05-11 19:00:01 +0300

    Changed class main. Added ability to check the correctness of the entered path to the file

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -5,5 +6,19 @@ public class Main {
         System.out.println("¬ведите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
         System.out.println("ƒлина текста: " + text.length());
+        int correctFile = 0;
+        while (true){
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (fileExists == false || !isDirectory == false) {
+                System.out.println("”казанный путь либо не существует, либо €вл€етс€ папкой, а не файлом.");
+
+            }else {
+                correctFile++;
+                System.out.println("Ёто файл номер: " + correctFile);
+            }
+        }
     }
 }
