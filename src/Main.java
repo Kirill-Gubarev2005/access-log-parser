fc6fa9216da14e710649e1ea8291ebb5d8531cde
Kirill Gubarev <klg05@mail.ru>
2025-06-22 23:01:27 +0300

    Changed class main. Added the ability to measure the number of lines and line parameters in a file

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
         int correctFile = 0;
-        while (true){
+        while (true) {
+            System.out.println("Введите путь к файлу: ");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists();
@@ -15,10 +16,46 @@ public class Main {
             if (fileExists == false || !isDirectory == false) {
                 System.out.println("Указанный путь либо не существует, либо является папкой, а не файлом.");
 
-            }else {
+            } else {
                 correctFile++;
                 System.out.println("Это файл номер: " + correctFile);
             }
+
+            try (FileReader fileReader = new FileReader(path);
+                 BufferedReader reader = new BufferedReader(fileReader)) {
+
+                int lineCount = 0;
+                int maxLength = 0;
+                long minLength = file.length();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    lineCount++;
+                    int length = line.length();
+                    if (length > 1024) {
+                        throw new LongLine("Строка #" + lineCount + " превышает максимально допустимую длину 1024 символа");
+                    }
+                    if (length > maxLength) {
+                        maxLength = length;
+                    }
+
+                    if (length < minLength) {
+                        minLength = length;
+                    }
+                }
+
+                System.out.println("Общее количество строк в файле: " + lineCount);
+                System.out.println("Длина самой длинной строки: " + maxLength);
+                System.out.println("Длина самой короткой строки: " + minLength);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
+    }
+}
+class LongLine extends RuntimeException {
+    public LongLine(String message) {
+        super(message);
     }
 }
