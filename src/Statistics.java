fd9ba978ef724f69e561ec9cb5a2af8e99b65941
Kirill Gubarev <klg05@mail.ru>
2025-07-01 17:18:07 +0300

    Added statistics by pages and operating systems

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,18 +1,28 @@
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Statistics {
     private long totalTraffic = 0;
     private LocalDateTime minTime = null;
     private LocalDateTime maxTime = null;
+    private final HashSet<String> pages = new HashSet<>();
+    private final HashMap<String, Integer> osStats = new HashMap<>();
 
     public Statistics() {}
 
     public void addEntry(LogEntry entry) {
         if (entry == null) return;
 
-        totalTraffic += entry.getDataSize();
+        if (entry.getResponseCode() == 200) {
+            pages.add(entry.getPath());
+        }
+        UserAgent userAgent = new UserAgent(entry.getUserAgent());
+        String os = userAgent.getOs();
+        osStats.put(os, osStats.getOrDefault(os, 0) + 1);
 
+        totalTraffic += entry.getDataSize();
         LocalDateTime entryTime = entry.getTime();
         if (minTime == null || entryTime.isBefore(minTime)) {
             minTime = entryTime;
@@ -22,6 +32,24 @@ public class Statistics {
         }
     }
 
+    public HashSet<String> getAllPages() {
+        return new HashSet<>(pages);
+    }
+
+    public HashMap<String, Double> getOsStatistics() {
+        HashMap<String, Double> result = new HashMap<>();
+        int total = 0;
+        for (Integer count : osStats.values()) {
+            total += count;
+        }
+        for (HashMap.Entry<String, Integer> entry : osStats.entrySet()) {
+            String os = entry.getKey();
+            int count = entry.getValue();
+            double percentage = (double) count / total;
+            result.put(os, percentage);
+        }
+        return result;
+    }
     public double getTrafficRate() {
         if (minTime == null || maxTime == null || minTime.equals(maxTime)) {
             return 0.0;
